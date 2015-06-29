35338478955fab000c709c5c939e6218782a120c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-06-29 16:06:33 -0700

    if LOCAL_JSON exists in the environment, beautify and spit out the JSON as /opt/phabricator/conf/local/local.json

--- phabricator/start.sh
+++ phabricator/start.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# Copy the JSON data if available as an environment variable
+if [ -n "${LOCAL_JSON}" ]; then
+  echo "${LOCAL_JSON}" | python -m json.tool > /tmp/local.json
+  if [ $? -ne 0 ]; then
+    echo "LOCAL_JSON is not a valid json"
+    exit 1
+  fi
+
+  mv /tmp/local.json /opt/phabricator/conf/local/local.json
+  chown www-data: /opt/phabricator/conf/local/local.json
+fi
+
 # Start the ssh server
 mkdir -p /usr/libexec /var/run/sshd
 /usr/sbin/sshd
