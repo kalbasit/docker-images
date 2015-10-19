c6d84e2dde7189dcb1fd00e1e35a74db52ed33f2
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-10-19 09:07:13 -0700

    tools/build.sh: fix a typo in the command name

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for i in */Rockerfile; do
-  push `dirname $i`
+  pushd `dirname $i`
   ./rocker build -var TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER -var COMMIT=$COMMIT --push --auth $DOCKER_USER:$DOCKER_PASS
   popd
 done
