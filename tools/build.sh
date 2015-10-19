8405878770690a7e04c0361f4504fba9e1f68776
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-10-19 09:05:38 -0700

    tools/build.sh: set the executable bit and run rocker from current pwd

--- tools/build.sh
+++ tools/build.sh
@@ -2,6 +2,6 @@
 
 for i in */Rockerfile; do
   push `dirname $i`
-  rocker build -var TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER -var COMMIT=$COMMIT --push --auth $DOCKER_USER:$DOCKER_PASS
+  ./rocker build -var TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER -var COMMIT=$COMMIT --push --auth $DOCKER_USER:$DOCKER_PASS
   popd
 done
