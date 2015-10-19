598a5c9c7e3a0bcc3780db7ac7b0d20bed5f59e0
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-10-19 09:02:46 -0700

    travis: build only Rockerfiles

---
+++ tools/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for i in */Rockerfile; do
+  push `dirname $i`
+  rocker build -var TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER -var COMMIT=$COMMIT --push --auth $DOCKER_USER:$DOCKER_PASS
+  popd
+done
